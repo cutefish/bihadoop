2bdd7046484d48e9a6e7864be5cf14d6e9982cf1
cutefish <cutefish.yx@gmail.com>
2012-08-23 19:21:51 -0400

    deleted:    src/java/mapred/org/apache/hadoop/map2/JobInProgress.java

--- src/java/mapred/org/apache/hadoop/map2/JobInProgress.java
+++
@@ -1,6 +0,0 @@
-/* Modifying JobInProgress.java
- */
-
-//Map<Node, Set<TaskInProgress>> taskPackCache;
-//Map<Node, List<TaskInProgress>> packedNonRunningMapTaskCache;
-//Map<Node, List<TaskInProgress>> runningMapTaskCache;
